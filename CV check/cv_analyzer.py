946ce2ca49d46b09b8b8eb20d6c7c2ad6f969b38
inosuras <hedi.trabelsi.0@gmail.com>
2026-01-15 16:14:41 +0100

    Delete CV check directory

--- CV check/cv_analyzer.py
+++
@@ -1,5 +0,0 @@
-# TODO: Lire le fichier CV
-# TODO: Lire le fichier job description  
-# TODO: Extraire les mots-clés importants
-# TODO: Calculer un score de correspondance
-# TODO: Afficher les résultats
